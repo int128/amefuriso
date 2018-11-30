94db674a7826925b378ecf678738930b6a59d75e
Hidetake Iwata <int128@gmail.com>
2018-11-30 13:59:54 +0900

    Add simple frontend

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # amefuriso
+
+A command for showing rainfall forecast using [YOLP weather API](https://developer.yahoo.co.jp/webapi/map/openlocalplatform/v1/weather.html).
+
+For example,
+
+```
+% ./amefuriso -client-id $YOLP_CLIENT_ID -lat 37.15918532117067 -lon 138.28260262402358
+2018/11/30 13:55:35 @{37.159185 138.2826}	observation	13:45	0.00 mm
+2018/11/30 13:55:35 @{37.159185 138.2826}	forecast	13:55	0.55 mm
+2018/11/30 13:55:35 @{37.159185 138.2826}	forecast	14:05	0.00 mm
+2018/11/30 13:55:35 @{37.159185 138.2826}	forecast	14:15	0.00 mm
+2018/11/30 13:55:35 @{37.159185 138.2826}	forecast	14:25	1.15 mm
+2018/11/30 13:55:35 @{37.159185 138.2826}	forecast	14:35	0.00 mm
+2018/11/30 13:55:35 @{37.159185 138.2826}	forecast	14:45	0.00 mm
+```
--- main.go
+++ main.go
@@ -1,34 +1,57 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
-	"os"
-
 	"github.com/int128/amefuriso/yolpweather"
+	"log"
 )
 
-func run() error {
-	clientID := os.Getenv("YOLP_CLIENT_ID")
+type options struct {
+	clientID  string
+	latitude  float64
+	longitude float64
+}
 
-	roppongi := yolpweather.Coordinates{
-		Latitude:  139.732293,
-		Longitude: 35.663613,
+func run(o options) error {
+	req := yolpweather.Request{
+		Coordinates: []yolpweather.Coordinates{{
+			Latitude:  o.latitude,
+			Longitude: o.longitude,
+		}},
 	}
-
-	c := yolpweather.New(clientID)
-	resp, err := c.Get(&yolpweather.Request{
-		Coordinates: []yolpweather.Coordinates{roppongi},
-	})
+	c := yolpweather.New(o.clientID)
+	resp, err := c.Get(&req)
 	if err != nil {
 		return fmt.Errorf("error while getting weather: %s", err)
 	}
-	log.Printf("%+v", resp)
+
+	for _, f := range resp.Payload.Feature {
+		coordinates, err := f.Geometry.Coordinates.Parse()
+		if err != nil {
+			log.Printf("Got invalid coordinates: %s", err)
+		}
+		for _, w := range f.Property.WeatherList.Weather {
+			t, err := w.Date.Parse()
+			if err != nil {
+				log.Printf("Got invalid date: %s", err)
+			}
+			log.Printf("@%v\t%s\t%s\t%.2f mm", coordinates, w.Type, t.Format("15:04"), w.Rainfall)
+		}
+	}
 	return nil
 }
 
 func main() {
-	if err := run(); err != nil {
+	var o options
+	flag.StringVar(&o.clientID, "client-id", "", "YOLP Client ID")
+	flag.Float64Var(&o.latitude, "lat", 35.663613, "Latitude")
+	flag.Float64Var(&o.longitude, "lon", 139.732293, "Longitude")
+	flag.Parse()
+	if o.clientID == "" {
+		log.Fatalf("Run with -client-id option")
+	}
+	if err := run(o); err != nil {
 		log.Fatalf("Error: %s", err)
 	}
 }
